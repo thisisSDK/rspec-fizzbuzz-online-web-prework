a5473159250992903c881bf147aa234134a5356e
sdk1270 <sdk1270@gmail.com>
2018-10-16 21:27:46 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 5 == 0 && int 3 == 0 
+  if int % 5 == 0 && int % 3 == 0 
     "FizzBuzz"
   elsif int % 3 == 0 
    "Fizz"
